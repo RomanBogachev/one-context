14b8fa1c1580c69aba41011b1bab08491f9469a5
Roman Bogachev <roman@bogachev.biz>
2015-12-25 22:14:34 +0300

    Check distr and version OS

--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ if [ -f /etc/os-release ];  then
 	VER=`cat /etc/os-release | grep '^VERSION_ID=' | sed "s/^VERSION_ID=//" | sed "s/'//g;s/\"//g"` # Get distributive version
 
 	# Ubuntu
-	if [[ $DIST=="Ubuntu" ]]; then
+	if [[ $DIST == "Ubuntu" ]]; then
 
 		# Ubuntu version 14.04
-		if [[ $VER=="14.04" ]]; then
+		if [[ $VER == "14.04" ]]; then
 
 			# Clean logs and etc
 			apt-get autoclean && apt-get autoremove && cat /dev/null > /var/log/alternatives.log && cat /dev/null > /var/log/auth.log && cat /dev/null > /var/log/boot.log && cat /dev/null > /var/log/bootstrap.log && cat /dev/null > /var/log/btmp && rm -f /var/log/dmesg.0 && rm -f /var/log/dmesg.1.gz && cat /dev/null > /var/log/dpkg.log && cat /dev/null > /var/log/faillog && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && cat /dev/null > /var/log/wtmp && rm -f /var/log/apt/* && rm -f /var/log/fsck/* && rm -f /var/log/installer/* && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/landscape/sysinfo.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && rm -f /var/log/unattended-upgrades && rm -f /var/log/upstart/* && rm -rf /var/tmp/* && rm -f /etc/ssh/ssh_host_dsa_key && rm -f /etc/ssh/ssh_host_dsa_key.pub && rm -f /etc/ssh/ssh_host_rsa_key && rm -f /etc/ssh/ssh_host_rsa_key.pub && rm -f /etc/ssh/ssh_host_key && rm -f /etc/ssh/ssh_host_key.pub && rm -f /etc/ssh/ssh_host_ed25519_key && rm -f /etc/ssh/ssh_host_ed25519_key.pub && history -cw
@@ -42,7 +42,7 @@ if [ -f /etc/os-release ];  then
 			# and the entire directory
 			rmdir `dirname $0`
 
-		elif [[ $VER=="15.10" ]]; then
+		elif [[ $VER == "15.10" ]]; then
 
 			# Clean logs and etc
 			apt-get autoclean && apt-get autoremove && cat /dev/null > /var/log/alternatives.log && cat /dev/null > /var/log/auth.log && cat /dev/null > /var/log/boot.log && cat /dev/null > /var/log/bootstrap.log && cat /dev/null > /var/log/btmp && rm -f /var/log/dmesg.0 && rm -f /var/log/dmesg.1.gz && cat /dev/null > /var/log/dpkg.log && cat /dev/null > /var/log/faillog && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && cat /dev/null > /var/log/wtmp && rm -f /var/log/apt/* && rm -f /var/log/fsck/* && rm -f /var/log/installer/* && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/landscape/sysinfo.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && rm -f /var/log/unattended-upgrades && rm -f /var/log/upstart/* && rm -rf /var/tmp/* && rm -f /etc/ssh/ssh_host_dsa_key && rm -f /etc/ssh/ssh_host_dsa_key.pub && rm -f /etc/ssh/ssh_host_rsa_key && rm -f /etc/ssh/ssh_host_rsa_key.pub && rm -f /etc/ssh/ssh_host_key && rm -f /etc/ssh/ssh_host_key.pub && rm -f /etc/ssh/ssh_host_ed25519_key && rm -f /etc/ssh/ssh_host_ed25519_key.pub && history -cw
@@ -75,7 +75,7 @@ if [ -f /etc/os-release ];  then
 		fi
 
 
-	elif [[ $DIST=="Debian GNU/Linux" ]]; then
+	elif [[ $DIST == "Debian GNU/Linux" ]]; then
 
 	# Clean logs and etc
 	apt-get autoclean && apt-get autoremove && cat /dev/null > /var/log/alternatives.log && cat /dev/null > /var/log/auth.log && cat /dev/null > /var/log/boot.log && cat /dev/null > /var/log/bootstrap.log && cat /dev/null > /var/log/btmp && rm -f /var/log/dmesg.0 && rm -f /var/log/dmesg.1.gz && cat /dev/null > /var/log/dpkg.log && cat /dev/null > /var/log/faillog && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && cat /dev/null > /var/log/wtmp && rm -f /var/log/apt/* && rm -f /var/log/fsck/* && rm -f /var/log/installer/* && cat /dev/null > /var/log/kern.log && cat /dev/null > /var/log/landscape/sysinfo.log && cat /dev/null > /var/log/lastlog && cat /dev/null > /var/log/syslog && cat /dev/null > /var/log/udev && rm -f /var/log/unattended-upgrades && rm -f /var/log/upstart/* && rm -rf /var/tmp/* && rm -f /etc/ssh/ssh_host_dsa_key && rm -f /etc/ssh/ssh_host_dsa_key.pub && rm -f /etc/ssh/ssh_host_rsa_key && rm -f /etc/ssh/ssh_host_rsa_key.pub && rm -f /etc/ssh/ssh_host_key && rm -f /etc/ssh/ssh_host_key.pub && rm -f /etc/ssh/ssh_host_ed25519_key && rm -f /etc/ssh/ssh_host_ed25519_key.pub && history -cw
